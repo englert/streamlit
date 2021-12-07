c9a7e293374d37a7d1b692bc4238ad7879027fb9
Englert Ervin  Brunó <englert@users.noreply.github.com>
2021-12-07 06:41:10 +0100

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,21 +1,14 @@
 import streamlit as st
 
-st.title('Counter Example')
+st.title('Counter')
 
-# Streamlit runs from top to bottom on every iteraction so
-# we check if `count` has already been initialized in st.session_state.
-
-# If no, then initialize count to 0
-# If count is already initialized, don't do anything
 if 'count' not in st.session_state:
 	st.session_state.count = 0
 
-# Create a button which will increment the counter
 increment = st.button('Increment')
 if increment:
     st.session_state.count += 1
 
-# A button to decrement the counter
 decrement = st.button('Decrement')
 if decrement:
     st.session_state.count -= 1
