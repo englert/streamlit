5be2d4b09c33bbf1fcda4c2beae7bbffeb3c758b
Englert Ervin  Brunó <englert@users.noreply.github.com>
2021-12-09 06:02:49 +0100

    Update counter.py

--- counter.py
+++ counter.py
@@ -116,3 +116,16 @@ name = st.text_input("Enter Your name", "Type Here ...")
 if(st.button('Submit')):
     result = name.title()
     st.success(result)
+
+	
+	# slider
+ 
+# first argument takes the title of the slider
+# second argument takes thr starting of the slider
+# last argument takes the end number
+level = st.slider("Select the level", 1, 5)
+ 
+# print the level
+# format() is used to print value
+# of a variable at a specific position
+st.text('Selected: {}'.format(level))
