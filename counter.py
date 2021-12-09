a772e0d406e59a4b601d5aacc758258546549527
Englert Ervin  Brunó <englert@users.noreply.github.com>
2021-12-09 05:49:47 +0100

    Update counter.py

--- counter.py
+++ counter.py
@@ -18,3 +18,9 @@ st.write('Count = ', st.session_state.count)
  
 # Title
 st.title("Hello GeeksForGeeks !!!")
+
+# Header
+st.header("This is a header")
+ 
+# Subheader
+st.subheader("This is a subheader")
