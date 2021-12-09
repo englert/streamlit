245b152cd3af5659123cd045ebf3de5d5987a952
Englert Ervin  Brunó <englert@users.noreply.github.com>
2021-12-09 05:54:16 +0100

    Update counter.py

--- counter.py
+++ counter.py
@@ -45,3 +45,13 @@ st.write("Text with write")
  
 # Writing python inbuilt function range()
 st.write(range(10))
+
+# Display Images
+ 
+# import Image from pillow to open images
+from PIL import Image
+img = Image.open("streamlit.png")
+ 
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img, width=200)
