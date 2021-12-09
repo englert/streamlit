851869019b75d78bad4b7293c40d30c534314382
Englert Ervin  Brunó <englert@users.noreply.github.com>
2021-12-09 20:25:18 +0100

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,5 +1,27 @@
 import streamlit as st
+import streamlit_pages.streamlit_pages import MultiPage
+def home():
+    st.write("Welcome to home page")
+    if st.button("Click Home"):
+        st.write("Welcome to home page")
 
+
+def about():
+    st.write("Welcome to about page")
+    if st.button("Click about"):
+        st.write("Welcome to About page")
+
+
+def contact():
+    st.write("Welcome to contact page")
+    if st.button("Click Contact"):
+        st.write("Welcome to contact page")
+
+
+# call app class object
+app = MultiPage()
+# Add pages
+app.add_page("Home",home)
 st.title('Counter')
 
 if 'count' not in st.session_state:
